d2ee15dbac02f7456804e1de20ca28954df66f89
jerryjames <jerryjames2001>
2024-08-04 22:45:34 +0530

    Sample array functions

---
+++ value_arrays.py
@@ -0,0 +1,11 @@
+import numpy as np
+print("Jerry James")
+uninit_array=np.empty(shape=(2,3))
+print("Un initialized array")
+print(uninit_array)
+one=np.ones(shape=(2,3))
+print("Araay with ones")
+print(one)
+zero=np.zeros(shape=(2,3))
+print("array with zeros")
+print(zero)
