f0030aedf756691adce2c75eae16eaa4fc86a6ca
JERRY JAMES <77136966+jerryjames2001@users.noreply.github.com>
2024-09-30 13:50:17 +0530

    Add files via upload

---
+++ iris_knn.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+from docutils.nodes import classifier
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report,confusion_matrix
+
+dataset = pd.read_csv("iris.csv")
+x = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,4].values
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.20)
+classifier = KNeighborsClassifier(n_neighbors=5)
+classifier.fit(x_train,y_train)
+y_pred = classifier.predict(x_test)
+print(classification_report(y_test,y_pred))
