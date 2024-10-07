a10dca070bca58d70b1602957cf79aa89557e4f0
JERRY JAMES <77136966+jerryjames2001@users.noreply.github.com>
2024-10-07 12:11:27 +0530

    navie baise using iris

---
+++ cycle_4/iris_naive_gaussian.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from PIL.Image import preinit
+from docutils.nodes import classifier
+
+dataset = pd.read_csv('iris.csv')
+x=dataset.iloc[:,:4].values
+y=dataset['variety'].values
+dataset.head(5)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.3)
+
+from sklearn.naive_bayes import GaussianNB
+classifier=GaussianNB()
+classifier.fit(x_train,y_train)
+y_pred=classifier.predict(x_test)
+print(y_pred)
+
+from sklearn.metrics import confusion_matrix
+cm=confusion_matrix(y_test,y_pred)
+print(cm)
+
+from sklearn.metrics import accuracy_score
+print("Accuracy: ",accuracy_score(y_test,y_pred))
+df=pd.DataFrame({'Real values ':y_test,"predicted value":y_pred})
+print(df)
